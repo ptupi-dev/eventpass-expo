d7760aa570b765c4ac553f1eaa3caeeb56eb983c
Potyguara Tupinambás <p.tupinambas.dev@gmail.com>
2026-07-20 14:03:29 -0300

    Persisted user auth and redirect

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,11 @@
 import { Redirect } from 'expo-router';
 
+import { useAuth } from '@/hooks/useAuth';
+import { AuthUser } from '@/types/auth';
+
 export default function IndexScreen() {
-  return <Redirect href="/login" />;
+  const { verifySession } = useAuth();
+  const { data: user = {} as AuthUser } = verifySession;
+
+  return <Redirect href={user?.['user-id'] ? '/home' : '/login'} />;
 }
